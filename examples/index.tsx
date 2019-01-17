d7125ce09a72e89d82f26061a33ea105a25bbf65
Kirill Alexander Khalitov <voronar@gmail.com>
2019-01-17 12:12:36 +0300

    1.0.3-alpha.2

--- examples/index.tsx
+++ examples/index.tsx
@@ -1,11 +1,30 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
-import { useReduxDispatcher } from '../src/index';
+import { useReduxDispatcher, StoreProvider, useReduxStateMapper } from '../src/index';
+import { createStore } from 'redux';
 
-const App = () => {
-  const a = useReduxDispatcher();
-  console.log(a);
-  return null;
+const App: React.FC = () => {
+  const dispatch = useReduxDispatcher();
+  const value = useReduxStateMapper((s: { a: number }) => s);
+  const a = useReduxStateMapper((s: { a: number }) => s.a);
+
+  React.useEffect(() => {
+    setInterval(() => {
+      dispatch({ type: 'action' });
+    }, 1000);
+  }, []);
+
+  return (
+    <div>
+      <div>{value.a}</div>
+      <div>{a}</div>
+    </div>
+  );
 };
 
-ReactDom.render(<App />, document.getElementById('app'));
+ReactDom.render(
+  <StoreProvider store={createStore((s: { a: number } = { a: 0 }) => ({ ...s, a: s.a + 1 }))}>
+    <App />
+  </StoreProvider>,
+  document.getElementById('app'),
+);
